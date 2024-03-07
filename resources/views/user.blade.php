037077454caf7393b4f3ea8fc4ecae3eaff48923
ichaputri <ichaputriana39@gmail.com>
2024-03-07 15:34:27 +0700

    Retreiving or Creating Models

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -28,11 +28,24 @@
                 <td>{{$data->level_id}}</td>
             </tr> --}}
 
-            <tr>
+            {{-- <tr>
                 <th>Jumlah Pengguna</th>
             </tr>
             <tr>
                 <td>{{ $userCount }}</td>
+            </tr> --}}
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
+            </tr>
+            
+            <tr>
+                <td>{{$data->user_id}}</td>
+                <td>{{$data->username}}</td>
+                <td>{{$data->nama}}</td>
+                <td>{{$data->level_id}}</td>
             </tr>
     </table>
 </body>
