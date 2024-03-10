11c9d23ab8ab30197dbff8353ac868309b5d5160
ichaputri <ichaputriana39@gmail.com>
2024-03-11 06:29:09 +0700

    Create, Read, Update, Delete (CRUD)

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,8 +4,8 @@
     <title>Data User</title>
 </head>
 <body>
-    <h1>Data User</h1>
-    <table border = "1" cellpadding="2" cellspacing="0">
+    {{-- <h1>Data User</h1>
+    <table border = "1" cellpadding="2" cellspacing="0"> --}}
         {{-- <tr>
             <th>ID</th>
             <th>Username</th>
@@ -34,7 +34,7 @@
             <tr>
                 <td>{{ $userCount }}</td>
             </tr> --}}
-            <tr>
+            {{-- <tr>
                 <th>ID</th>
                 <th>Username</th>
                 <th>Nama</th>
@@ -46,7 +46,27 @@
                 <td>{{$data->username}}</td>
                 <td>{{$data->nama}}</td>
                 <td>{{$data->level_id}}</td>
-            </tr>
+            </tr> --}}
+
+            <h1>Data User</h1>
+            <a href="/user/tambah"> + Tambah User</a>
+            <table border="1" cellpadding="2" cellspacing="0">
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Nama</th>
+                    <th>ID Level Pengguna</th>
+                </tr>
+                @foreach ($data as $d)
+                    <tr>
+                        <td>{{$d->user_id}}</td>
+                        <td>{{$d->username}}</td>
+                        <td>{{$d->nama}}</td>
+                        <td>{{$d->level_id}}</td>
+                        <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Hapus</a></td>
+                    </tr>
+        @endforeach
+            </table>
     </table>
 </body>
 </html>
