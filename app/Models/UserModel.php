983e8a35d66f31a7a3619371de29e5f6e2ccbe5a
ichaputri <ichaputriana39@gmail.com>
2024-04-25 15:19:19 +0700

    JS 10 - Create Restful Api Register

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,9 +7,19 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Model implements AuthenticatableContract
+class UserModel extends Model implements AuthenticatableContract, JWTSubject
 {
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    
     use Authenticatable;
     protected $table = 'm_user'; //mendefinidikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang digunakan
@@ -19,8 +29,8 @@ class UserModel extends Model implements AuthenticatableContract
      * @var array
      */
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
-    public function level():BelongsTo{
+    public function level(): BelongsTo
+    {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
-
 }
