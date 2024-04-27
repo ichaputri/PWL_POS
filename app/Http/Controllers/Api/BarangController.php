6555375998dd26438417f2f3e2fbc96c14da4c18
ichaputri <ichaputriana39@gmail.com>
2024-04-27 14:46:41 +0700

    JS - 10 | Implementasi Crud UserController, KategoriController. dan BarangController pada API

---
+++ app/Http/Controllers/Api/BarangController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\BarangModel;
+use Illuminate\Http\Request;
+
+class BarangController extends Controller
+{
+    public function index()
+    {
+        return BarangModel::all();
+    }
+
+    public function store(Request $request)
+    {
+        $barang = BarangModel::create($request->all());
+        return response()->json($barang, 201);
+    }
+
+    public function show(BarangModel $barang)
+    {
+        return $barang; // Secara otomatis, Laravel akan mengonversi objek barang ke JSON
+    }
+
+    public function update(Request $request, BarangModel $barang)
+    {
+        $barang->update($request->all());
+        return $barang;
+    }
+
+    public function destroy(BarangModel $barang)
+    {
+        $barang->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data terhapus',
+        ]);
+    }
+}
---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return UserModel::all();
+    }
+
+    public function store(Request $request)
+    {
+        $user = UserModel::create($request->all());
+        return response()->json($user, 201);
+    }
+
+    public function show(UserModel $user)
+    {
+        return $user; // Secara otomatis, Laravel akan mengonversi objek User ke JSON
+    }
+
+    public function update(Request $request, UserModel $user)
+    {
+        $user->update($request->all());
+        return $user;
+    }
+
+    public function destroy(UserModel $user)
+    {
+        $user->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data terhapus',
+        ]);
+    }
+}
