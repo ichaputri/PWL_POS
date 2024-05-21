22a5afeaa8e7a8119b7db9294f2e0933be354b08
ichaputri <ichaputriana39@gmail.com>
2024-05-21 21:21:05 +0700

    JS - 12 | Membuat Symlink

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -17,8 +17,12 @@ class FileUploadController extends Controller
         ]);
         $extfile = $request->berkas->getClientOriginalName();
         $namaFile = 'web-'.time().".".$extfile;
-        $path = $request->berkas->storeAs('uploads', $namaFile);
+        $path = $request->berkas->storeAs('public', $namaFile);
+
+        $pathBaru = asset('storage/'.$namaFile);
         echo "Proses upload berhasil. File berada di : $path";
+        echo "<br>";
+        echo "Tampilkan link : <a href='$pathBaru'>$pathBaru</a>";
         // $path = $request->berkas->store('uploads');
         // dump($request->berkas);
         // return "Pemrosesan file upload disini";
