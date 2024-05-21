ae1570e278891bc23ff5437e7f0a6d9fcf1ecb76
ichaputri <ichaputriana39@gmail.com>
2024-05-21 22:32:48 +0700

    JS - 12 | Method Move

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -17,9 +17,13 @@ class FileUploadController extends Controller
         ]);
         $extfile = $request->berkas->getClientOriginalName();
         $namaFile = 'web-'.time().".".$extfile;
-        $path = $request->berkas->storeAs('public', $namaFile);
 
-        $pathBaru = asset('storage/'.$namaFile);
+        // $path = $request->berkas->storeAs('public', $namaFile);
+        $path = $request->berkas->move('gambar', $namaFile);
+        $path = str_replace("\\","//",$path);
+        echo "Variabel path berisi: $path <br>";
+
+        $pathBaru = asset('gambar/'.$namaFile);
         echo "Proses upload berhasil. File berada di : $path";
         echo "<br>";
         echo "Tampilkan link : <a href='$pathBaru'>$pathBaru</a>";
